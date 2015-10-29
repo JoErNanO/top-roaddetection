867a10fe413cb13f7aa71e039671e174e47bd831
Francesco Giovannini <joernano@gmail.com>
2015-10-29 16:41:24 +0100

    [TRAVIS] Fix travis build.

--- .travis/push-doxygen-to-gh-pages.sh
+++ .travis/push-doxygen-to-gh-pages.sh
@@ -6,6 +6,8 @@
 
 # Running 3 builds - save javadoc only for one of them. Chose scala 2.11.7.
 
+echo -e "Travis scala version is: $TRAVIS_SCALA_VERSION"
+
 if [ "$TRAVIS_REPO_SLUG" == "JoErNanO/top-roaddetection" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_BUILD_TYPE" == "Release" && "$TRAVIS_SCALA_VERSION" == "2.11.7" ]; then
 
     echo -e "Publishing scaladoc. \n"
