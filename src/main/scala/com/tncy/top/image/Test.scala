f101cca0a2f656a9057a4a4250d17c1a4e4b7568
Francesco Giovannini <joernano@gmail.com>
2015-10-29 14:49:52 +0100

    [IW] Create function to display image in JFrame in testing script.

--- src/main/scala/com/tncy/top/image/Test.scala
+++ src/main/scala/com/tncy/top/image/Test.scala
@@ -4,34 +4,62 @@ import com.tncy.top.image;
 
 import java.io.File
 import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.FlowLayout; 
 
 
 object Test extends App {
+  val imgpath : String = "/home/fgiovann/Documents/TelecomNancy/telecomnancy/top/RoadDetection/images/";
+  var fileName : String = imgpath + "Tears_In_Rain_600px.png";
+  //var imageLoader = AerialImage;
   
-  override def main(args : Array[String]) {
-    val imgpath : String = "/home/fgiovann/Documents/TelecomNancy/telecomnancy/top/RoadDetection/images/";
-    var fileName : String = imgpath + "Tears_In_Rain_600px.png";
-    //var imageLoader = AerialImage;
-    
-    // Load wrapped image
-    var wrappedImage : ImageWrapper = new ImageWrapper(fileName);
-    var image2D : Array[Array[Int]] = wrappedImage.getImage();
-    for (row <- 0 to 40) {
-      for (col <-0 to 80) {
-        image2D(row)(col) = 0x000000FF;
-      }
+  // Load wrapped image
+  var wrappedImage : ImageWrapper = new ImageWrapper(fileName);
+  var image2D : Array[Array[Int]] = wrappedImage.getImage();
+  //println(wrappedImage);
+  println("The image height is: " + wrappedImage.height + " px.");
+  println("The image width is: " + wrappedImage.width + " px.");
+  
+  // Show loaded image
+  val sourceimage = new File(fileName) : File;
+  var loadedImage = ImageIO.read(sourceimage); 
+  showImage(loadedImage, "Loaded Image"); 
+  
+  // Modify image
+  for (row <- 0 to 40) {
+    for (col <-0 to 80) {
+      image2D(row)(col) = 0x000000FF;
     }
-    wrappedImage.saveImage(imgpath + "Tears_In_Rain_modded.png");
-    println(wrappedImage);
-    println(wrappedImage.height);
-    println(wrappedImage.width);
+  }
+
+  // Save image
+  val outPath = imgpath + "Tears_In_Rain_modded.png";
+  wrappedImage.saveImage(outPath);
+  
+  // Show saved image
+  val outimage = new File(outPath) : File;
+  var savedImage = ImageIO.read(outimage); 
+  showImage(savedImage, "Saved Image"); 
+
+  /** Display the given image in a JFrame.
+   *  
+   *  @author Francesco Giovannini
+   */
+  private def showImage(image : BufferedImage, title : String) : Boolean = {
     
-    val sourceimage = new File(fileName) : File;
-    var loadedImage = ImageIO.read(sourceimage);
-    
-    //imageLoader.showImage(loadedImage, "Loaded Image"); 
-    
-    //var image2D = imageLoader.loadImage(fileName);
-    //imageLoader.saveImage("/home/fgiovann/Documents/TelecomNancy/telecomnancy/top/RoadDetection/images/anarchy_200px_rebuilt.jpg", image2D);
+    var frame = new JFrame() : JFrame;
+    frame.setTitle(title);
+    frame.getContentPane().setLayout(new FlowLayout());
+    frame.getContentPane().add(new JLabel(new ImageIcon(image)));
+    frame.pack();
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    return true
   }
 }
